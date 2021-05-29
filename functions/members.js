f4f2008fe62d7a2466a965f1f051be3a6ce80ec7
noral <nora.lefeuvre@gmail.com>
2021-05-29 20:16:04 +0200

    add members functions

--- functions/members.js
+++ functions/members.js
@@ -6,17 +6,26 @@ const service = new MemberService({ client })
 exports.handler = async (event, context) => {
 	console.log('Function `members` invoked')
 
-	if (event.httpMethod !== 'GET') {
+	if (event.httpMethod !== 'GET' || event.httpMethod !== 'POST') {
 		return { statusCode: 405, headers, body: 'Method Not Allowed' }
 	}
 
 	try {
-		const members = await service.getMembers()
-		return {
-			statusCode: 200,
-			headers,
-			body: JSON.stringify(members),
-		}
+    if (event.httpMethod === 'GET') {
+      const members = await service.getMembers()
+      return {
+        statusCode: 200,
+        headers,
+        body: JSON.stringify(members),
+      }
+    }
+    if (event.httpMethod === 'POST') {
+      service.addMember('test01');
+      return {
+        statusCode: 200,
+        headers
+      }
+    }
 	} catch (error) {
 		console.log('error', error)
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
@@ -9,18 +10,26 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent {
   title = 'dev-tech-challenge-app';
   members:string[] = [];
+  checkoutForm = this.formBuilder.group({
+    name: ''
+  });
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private formBuilder: FormBuilder) {}
 
 	ngOnInit() {
 		this.http.get('https://upbeat-lamport-af75f5.netlify.app/.netlify/functions/members').subscribe((response) => {
-			console.log('response: ', response);
-      console.log('test');
-      console.log(Object.values(response));
       Object.values(response).forEach(element => {
         console.log(element.data.name);
         this.members.push(element.data.name);
       });
 		})
 	}
+
+  onSubmit(): void {
+    this.http.post('https://upbeat-lamport-af75f5.netlify.app/.netlify/functions/members', this.checkoutForm.value.name).subscribe((response) => {
+      console.log(response);
+    })
+    console.log('Done: ', this.checkoutForm.value.name);
+    this.checkoutForm.reset();
+  }
 }
